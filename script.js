2e5641e61652fa339ebf90e87d24f1e26730a543
Dusan <shandoo91@gmail.com>
2022-09-20 16:49:24 +0200

    added initial fetch script and events

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello!');
+const endpoint = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15';
+const games = [];
+const inputText = document.querySelector('.text');
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => games.push(...data))
+
+
+function searchData(input, value) {
+    return value.filter (game => {
+        const regex = new RegExp (input, 'gi');
+        return game.title.match(regex);
+    });
+}
+
+function displayData() {
+    const matchedArray = searchData(this.value, games);
+    console.log(matchedArray);
+}
+
+inputText.addEventListener('keyup', displayData);
+inputText.addEventListener('change', displayData);
